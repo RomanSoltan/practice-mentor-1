495130d4d8f149783dda84a9adc6babffb7d6258
Roman Soltan <romabandit88@gmail.com>
2024-12-14 17:12:57 +0200

    add prompt.js

---
+++ src/js/prompt.js
@@ -0,0 +1,37 @@
+// ЗАВДАННЯ 2
+
+// - Використовуй prompt та повертай значення звідти.
+// - Створи функцію, яка буде набувати значення з prompt і повертатиме проміс.
+// Якщо значення не є числом, відхиляй проміс та логіруй "error".
+// Якщо значення парне, вирішуй проміс та повертай "even" через 1 секунду.
+// Якщо значення не парне, вирішуй проміс та повертай "odd" через 2 секунди.
+
+const promptValue = prompt("Enter value");
+
+function getPrompt(value) {
+  // повертаємо проміс з функції
+  return new Promise((resolve, reject) => {
+    // приведемо дані до числа
+    const valueNum = +value;
+    // використаємо метод isNaN для перевірки не числа
+    if(isNaN(valueNum)) {
+      reject("error");
+    }
+    // перевірка чи парне
+    if(!(valueNum % 2)) {
+      setTimeout(() => {
+        resolve("even");
+      }, 1000);
+    }
+    // перевірка чи непарне
+    if(valueNum % 2) {
+      setTimeout(() => {
+        resolve("odd");
+      }, 2000);
+    }
+  })
+} 
+
+getPrompt(promptValue)
+  .then(data => console.log(data))
+  .catch(error => console.log(error.message));
