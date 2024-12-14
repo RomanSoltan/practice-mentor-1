45465590da4c790d17b8576e0769e48193c966d7
RomanSoltan <137917472+RomanSoltan@users.noreply.github.com>
2024-12-14 19:13:36 +0000

    Deploying to gh-pages from @ RomanSoltan/practice-mentor-1@e10378097cdf2625dd25b96d27a05240a3337c30 🚀

---
+++ count-age.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";function o(a){const t=new Date,e=new Date(a);let n=t.getFullYear()-e.getFullYear();const l=t.getMonth()-e.getMonth(),c=t.getDate()-e.getDate();return(l<0||c<0&&!l)&&(n-=1),n}console.log(o("1988-08-13"));console.log(o("1988-12-15"));console.log(o("1988-11-13"));
+//# sourceMappingURL=count-age.js.map
