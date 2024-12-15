b9fb0d928e2999daaace9574a45d853e3d1a82eb
Roman Soltan <romabandit88@gmail.com>
2024-12-15 23:46:04 +0200

    add loader, izitoast for err

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -9,6 +9,7 @@ import renderImages from './render-function';
 
 const galleryEl = document.querySelector('.gallery');
 const form = document.querySelector('.js-search-form');
+const loader = document.querySelector(".loader")
 const container = document.getElementById('tui-pagination-container');
 
 form.addEventListener('submit', handleSubmit);
@@ -22,12 +23,24 @@ const pagination = new Pagination(container, {
 const page = pagination.getCurrentPage();
 
 const api = new UnsplashAPI();
-api.getPopularPhotos(page).then(data => {
-  console.log(data);
-  galleryEl.innerHTML = renderImages(data.results);
-  // додамо до пагінації загальну кількісь фото
-  pagination.reset(data.total);
-});
+api.getPopularPhotos(page)
+  .then(data => {
+    console.log(data);
+    galleryEl.innerHTML = renderImages(data.results);
+    // додамо до пагінації загальну кількісь фото
+    pagination.reset(data.total);
+  })
+  .catch(error => 
+    iziToast.show({
+      title: 'X',
+      message: `${error}` || "Oooopps",
+      position: 'center',
+      color: 'red',
+    })
+  )
+  .finally(() => {
+    loader.classList.add("visually-hidden");
+  });
 
 // повісимо слухача події на пагінацію
 pagination.on('afterMove', handlePagination);
@@ -36,17 +49,37 @@ pagination.on('afterMove', handlePagination);
 function handlePagination(event) {
   // номер сторінки, який натиснули в пагінації
   const currentPage = event.page;
+  loader.classList.remove("visually-hidden")
   // зробимо запит з номером сторінки, який натиснули в пагінації
   api
     .getPopularPhotos(currentPage)
-    .then(data => (galleryEl.innerHTML = renderImages(data.results)));
+    .then(data => galleryEl.innerHTML = renderImages(data.results))
+    .catch(error => 
+      iziToast.show({
+        title: 'X',
+        message: `${error}` || "Oooopps",
+        position: 'center',
+        color: 'red',
+      })
+    )
+    .finally(() => loader.classList.add("visually-hidden"));
 }
 
 // пагінація для фото, які прийшли за запитом користувача
 function handleSubmitPagination(event) {
+  loader.classList.remove("visually-hidden")
   const currentPage = event.page;
   api.getPhotos(currentPage)
     .then(data => galleryEl.innerHTML = renderImages(data.results))
+    .catch(error => 
+      iziToast.show({
+        title: 'X',
+        message: `${error}` || "Oooopps",
+        position: 'center',
+        color: 'red',
+      })
+    )
+    .finally(() => loader.classList.add("visually-hidden"));
 }
 
 function handleSubmit(event) {
@@ -69,10 +102,40 @@ function handleSubmit(event) {
 
   // передамо дані, які ввів користувач у
   api.query = inputValue;
+
+  loader.classList.remove("visually-hidden");
   // зробимо запит на сервер викликавши метод getPhotos
   api.getPhotos(page).then(data => {
+    if(!data.results.length) {
+      iziToast.show({
+        title: 'X',
+        message: "Oooopps, there is no photo",
+        position: 'center',
+        color: 'red',
+      });
+      return;
+    }
+
+    if(data.total <= 12) {
+      container.classList.add("visually-hidden");
+    } else {
+      container.classList.remove("visually-hidden")
+    }
+
     galleryEl.innerHTML = renderImages(data.results);
     pagination.reset(data.total);
+  })
+  .catch(error => 
+    iziToast.show({
+      title: 'X',
+      message: `${error}` || "Oooopps",
+      position: 'center',
+      color: 'red',
+    })
+  )
+  .finally(() => {
+    loader.classList.add("visually-hidden");
+    event.target.reset();
   });
   // повісимо слухача події
   pagination.on("afterMove", handleSubmitPagination);
