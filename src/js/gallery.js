ee6e17ffe589f5a36a1cad3576e06570385983e8
Roman Soltan <romabandit88@gmail.com>
2024-12-15 15:47:30 +0200

    add rende-function.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,8 +1,11 @@
-import UnsplashAPI from "./UnsplashAPI";
+import UnsplashAPI from './UnsplashAPI';
+import renderImages from './render-function';
+
+const galleryEl = document.querySelector('.gallery');
 
 const api = new UnsplashAPI();
 
-api.getPopularPhoto(1)
-  .then(data => {
-    console.log(data);
-  })
+api.getPopularPhoto(1).then(data => {
+  console.log(data);
+  galleryEl.innerHTML = renderImages(data);
+});
---
+++ src/js/render-function.js
@@ -0,0 +1,25 @@
+const renderImages = (images) => {
+  return images
+  .map(
+      image => `
+      <li class="gallery__item">
+        <img class="gallery-img" src="${image.urls.small}" alt="${image.alt_description}">
+      </li>
+    `
+    )
+    .join('');
+};
+
+export default renderImages;
+
+// export default function renderImages(arr) {
+//   return arr
+//     .map(
+//       ({ urls: { small }, alt_description }) => `
+//       <li class="gallery__item">
+//         <img class="gallery-img" src="${small}" alt="${alt_description}" />
+//       </li>
+//   `
+//     )
+//     .join('');
+// }
