5770590bec999692b77bfd97919ea373f773517f
Roman Soltan <romabandit88@gmail.com>
2024-12-15 14:33:54 +0200

    add unsplash.js

---
+++ src/js/UnsplashAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export default class UnsplashAPI {
+  #BASE_URL = 'https://api.unsplash.com/photos';
+  #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
+
+  async getPopularPhoto(page) {
+    const params = new URLSearchParams({
+      query: "popular",
+      page,
+      per_page: 12,
+      orientation: "portrait",
+      client_id: this.#API_KEY,
+    });
+
+    const { data } = await axios(`${this.#BASE_URL}?${params}`);
+    return data;
+  }
+}
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,1 +1,8 @@
+import UnsplashAPI from "./UnsplashAPI";
 
+const api = new UnsplashAPI();
+
+api.getPopularPhoto(1)
+  .then(data => {
+    console.log(data);
+  })
