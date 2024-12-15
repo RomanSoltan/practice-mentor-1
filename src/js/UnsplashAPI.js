8f85e21e0644d3fd03d730211d33dc3226d84667
Roman Soltan <romabandit88@gmail.com>
2024-12-15 22:32:12 +0200

    user-request pagination

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -3,7 +3,9 @@ import axios from "axios";
 export default class UnsplashAPI {
   #BASE_URL = 'https://api.unsplash.com/search/photos';
   #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
+  #query = ""
 
+  // метод для популярних фото
   async getPopularPhotos(page) {
     const params = new URLSearchParams({
       query: "popular",
@@ -17,4 +19,22 @@ export default class UnsplashAPI {
     return data;
   }
 
+  // метод для запиту зображеннь по пошуку
+  async getPhotos(page) {
+    const params = new URLSearchParams({
+      query: this.#query,
+      page,
+      per_page: 12,
+      orientation: "portrait",
+      client_id: this.#API_KEY,
+    });
+
+    const { data } = await axios(`${this.#BASE_URL}?${params}`);
+    return data;
+  }
+
+  // сетер для присвоєння значення в query
+  set query(newQuery) {
+    this.#query = newQuery; 
+  }
 }
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,21 +1,27 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import err from '../img/javascript.svg';
 
 import UnsplashAPI from './UnsplashAPI';
 import renderImages from './render-function';
 
 const galleryEl = document.querySelector('.gallery');
-const api = new UnsplashAPI();
+const form = document.querySelector('.js-search-form');
 const container = document.getElementById('tui-pagination-container');
+
+form.addEventListener('submit', handleSubmit);
+
 const pagination = new Pagination(container, {
   totalItems: 0,
   itemsPerPage: 12,
   visiblePages: 5,
   page: 1,
 });
-
 const page = pagination.getCurrentPage();
 
+const api = new UnsplashAPI();
 api.getPopularPhotos(page).then(data => {
   console.log(data);
   galleryEl.innerHTML = renderImages(data.results);
@@ -24,11 +30,50 @@ api.getPopularPhotos(page).then(data => {
 });
 
 // повісимо слухача події на пагінацію
-pagination.on('afterMove', event => {
+pagination.on('afterMove', handlePagination);
+
+// пагінація для популярних фото
+function handlePagination(event) {
   // номер сторінки, який натиснули в пагінації
   const currentPage = event.page;
   // зробимо запит з номером сторінки, який натиснули в пагінації
   api
     .getPopularPhotos(currentPage)
     .then(data => (galleryEl.innerHTML = renderImages(data.results)));
-});
+}
+
+// пагінація для фото, які прийшли за запитом користувача
+function handleSubmitPagination(event) {
+  const currentPage = event.page;
+  api.getPhotos(currentPage)
+    .then(data => galleryEl.innerHTML = renderImages(data.results))
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const inputValue = form.elements.query.value.trim();
+  console.log(inputValue);
+
+  if (!inputValue) {
+    iziToast.warning({
+      title: 'Caution',
+      message: 'The field is empty, please type your request',
+      position: 'topRight',
+      iconUrl: `${err}`,
+    });
+    return;
+  }
+  // відпшемося від слухача події, щоб не було збільшення запитів
+  pagination.off("afterMove", handlePagination);
+  pagination.off("afterMove", handleSubmitPagination);
+
+  // передамо дані, які ввів користувач у
+  api.query = inputValue;
+  // зробимо запит на сервер викликавши метод getPhotos
+  api.getPhotos(page).then(data => {
+    galleryEl.innerHTML = renderImages(data.results);
+    pagination.reset(data.total);
+  });
+  // повісимо слухача події
+  pagination.on("afterMove", handleSubmitPagination);
+}
